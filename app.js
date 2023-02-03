e95f3da94c6cea8d150d4cf733173a44f4f739fa
Muhammad Umar <123749774+14umar@users.noreply.github.com>
2023-02-03 09:19:35 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+// Question # 1
+// var name = ("Hello umar")
+// alert(name)
+
+// Question # 2
+// var name = ("Error! Please enter a valid password.")
+// alert(name)
+
+// Question # 3
+//  var name = ("Welcome to JS Land..\n Happy Coding!")
+//  alert(name)
+
+// Question # 4
+ var name = ("Welcome to JS Land..")
+ alert(name)
+ var namee = ("Happy Coding!")
+ alert(namee)
+
+
+
