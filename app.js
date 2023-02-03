f8ad650b62e37ea40a90787ea433ecb6edd3bfa9
Muhammad Umar <123749774+14umar@users.noreply.github.com>
2023-02-03 09:23:31 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,55 @@
-// Question # 1
-// var name = ("Hello umar")
-// alert(name)
+// Question 1
+// var username;
+ 
 
-// Question # 2
-// var name = ("Error! Please enter a valid password.")
-// alert(name)
+// Question 2
+// var myname = ("Muhammad Umar");
+// alert(myname)
 
-// Question # 3
-//  var name = ("Welcome to JS Land..\n Happy Coding!")
-//  alert(name)
+// Question 3a
+// var num1 = ("Done");
+// alert(num1)
 
-// Question # 4
- var name = ("Welcome to JS Land..")
- alert(name)
- var namee = ("Happy Coding!")
- alert(namee)
+// Question 3b
+// var num1 = "Hello World";
+// alert(num1)
 
+// Question 4
+// var num1 = ("jhone Done");
+// var num2 = ("15 years old")
+// var num3 = ("certified mobile app development")
 
+// alert(num1);
+// alert(num2);
+// alert(num3);
+
+// Question 5
+// var name;
+// name = "pizza";
+// alert(name);
+// var name;
+// name = "pizz";
+// alert(name);
+// name = "piz";
+// alert(name);
+// name = "pi";
+// alert(name);
+// name = "p";
+// alert(name);
+
+// Question 6
+// var email;
+// email = "mohammadumarazaz090@gmail.com";
+// alert ("my email is " + email)
+
+// Question 7
+// var book = "A smarter way to learn javascrpt"
+// alert(book)
+
+// Question 8
+// document.write("Yah! I can write HTML content through javascript")
+
+// Question 9
+// var num1 = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+// alert(num1)
 
